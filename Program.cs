f5f4ec8263bc726227c3a996e79d8d80aaad83dd
jshwangbo <jshwangbo3411@gmail.com>
2026-03-11 10:32:54 +0900

    chore(init): initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
